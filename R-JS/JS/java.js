94cb6a68b59a287ee545923b6bccb194570a1b0c
RamzanDamaev <114807195+RamzanDamaev@users.noreply.github.com>
2022-10-01 14:41:06 +0300

    Add files via upload

---
+++ R-JS/JS/java.js
@@ -0,0 +1,16 @@
+let a = 9;
+const b = 14;
+a + b;
+console.log(a + b);
+
+let name = prompt("Ваше имя?")
+let age = prompt("Ваш возраст?")
+console.log(`Меня зовут ${name} мне полных ${age} лет` );
+
+alert('Добро пожаловать!');  
+
+if(a < b) {
+    console.log('значение контраста больше 10')}
+     else{
+        console.log('значение контраста меньше 10')
+     }
